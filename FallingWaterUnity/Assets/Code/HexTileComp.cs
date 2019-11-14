1d68df6622fef9a1dba318d84c835eb84d72376e
Spencer <spencerjohnson714@gmail.com>
2019-11-13 20:50:02 -0800

    Alright new tile rotation mostly works, just needs to feel better

--- FallingWaterUnity/Assets/Code/HexTileComp.cs
+++ FallingWaterUnity/Assets/Code/HexTileComp.cs
@@ -63,41 +63,49 @@ public class HexTileComp : MonoBehaviour
       hexPos_ScreenSpace.z = 0;
       Vector3 mousePos_HexSpace = Input.mousePosition - hexPos_ScreenSpace;
 
-      float currentAngleCW = ((transform.rotation.eulerAngles.z + 360) % 360);
-      currentAngleCW = (int)(-currentAngleCW + 90) % 360;
+      //float currentAngleCW = ((transform.rotation.eulerAngles.z + 360) % 360);
+      //currentAngleCW = (int)(-currentAngleCW) % 360;
+      float currentAngleCW = -transform.rotation.eulerAngles.z;
+      if (currentAngleCW < 0)
+      {
+        currentAngleCW = 360 - currentAngleCW;
+      }
 
-      float targetAngleCW = Vector3.Angle(mousePos_HexSpace, Vector3.up);
-      if (Mathf.Sign(mousePos_HexSpace.x) == -1)
+      float targetAngleCW = Vector3.Angle(mousePos_HexSpace, Vector3.right);
+      if (mousePos_HexSpace.y > 0)
       {
         targetAngleCW = 360 - targetAngleCW;
       }
 
-      if (m_bSelected)
-        print(mousePos_HexSpace);
-      if (m_bSelected && mousePos_HexSpace.magnitude/Screen.width > (15 / Screen.width)/*est hex screen space size sq*/)
-      {
-        m_targetSlice = (int)targetAngleCW / 60;
-      }
-
-      float targetZAngleCW = m_targetSlice * 60 + 30;
-      float targetZAngleReal = -1 * (targetZAngleCW - 90);
-      if(m_bSelected)
-        print("currentAngleCW:" + currentAngleCW + "\ttargetHexSlice: " + m_targetSlice + "\ttargetAngleCW: " + targetAngleCW + "\ttargetZAngleReal: " + targetZAngleReal);
-
-      Vector3 ZRotNormalVec = new Vector3(-Mathf.Sin((targetZAngleCW - 90) * Mathf.Deg2Rad), Mathf.Cos((targetZAngleCW - 90) * Mathf.Deg2Rad), 0.0f);
-      int rotDir = (int)Mathf.Sign((int)Vector3.Dot(mousePos_HexSpace, ZRotNormalVec));
-      float targetRotDelta = rotDir * Mathf.DeltaAngle(targetZAngleReal, transform.rotation.eulerAngles.z);
       
-
-      if (targetRotDelta > 10)
+      if (m_bSelected ) //&& mousePos_HexSpace.magnitude/Screen.width > (30 / Screen.width)/*est hex screen space size sq*/)
       {
-        transform.Rotate(-Vector3.forward, targetRotDelta * .2f, Space.Self);
+        m_targetSlice = ((Mathf.FloorToInt(targetAngleCW) + 30) % 360) / 60;
+      }
+
+      float targetZAngleCW = m_targetSlice * 60;
+      float targetZAngleReal = -1 * (targetZAngleCW);
+      //if(m_bSelected)
+        //print(/*"currentAngleCW:" + currentAngleCW +*/ "\ttargetHexSlice: " + m_targetSlice + "\ttargetAngleCW: " + targetAngleCW + "\ttargetZAngleReal: " + targetZAngleReal);
+
+      Vector3 hexRot2D = new Vector3(Mathf.Cos(currentAngleCW * Mathf.Deg2Rad), Mathf.Sin(currentAngleCW * Mathf.Deg2Rad), 0.0f);
+      Vector3 ZRotNormalVec = new Vector3(Mathf.Sin((targetZAngleReal) * Mathf.Deg2Rad), -Mathf.Cos((targetZAngleReal) * Mathf.Deg2Rad), 0.0f);
+      int rotDir = (int)Mathf.Sign((int)Vector3.Dot(hexRot2D, ZRotNormalVec));
+
+      float targetRotDelta = rotDir * Mathf.DeltaAngle(targetZAngleReal, currentAngleCW);
+      
+      if (Mathf.Abs(targetRotDelta) > 5)
+      {
+        transform.Rotate(-Vector3.forward, targetRotDelta * .3f, Space.Self);
 
       }
-      else //snap to desired hex
+      else if(m_bSelected) //snap to desired hex slice
       {
         //print("snap!");
         transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, targetZAngleReal);
+        //int index = Random.Range(0, m_rotateSounds.Length);
+        //audioSource.pitch = .9f + (index % 3 * .1f);
+        //audioSource.PlayOneShot(m_rotateSounds[index], .5f);
       }
     }
   }
